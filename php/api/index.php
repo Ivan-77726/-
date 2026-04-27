a1e80621a1e16119a29faf1aa1567094db306e8c
Daria Dukh <dukh_077@mail.ru>
2026-04-27 12:11:07 +0600

    1234

---
+++ php/api/index.php
@@ -0,0 +1,13 @@
+php//api/index.php
+<!doctype html>
+<html lang="ru">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Пример</title>
+</head>
+<body>
+<?php
+echo "<h2>Эндпойнт API php</h2>";
+?>
+</body>
+</html>
